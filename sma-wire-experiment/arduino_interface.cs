9cf6919b3fbf85dc5731bc4793edce77569a4edb
azizimalek <azizimalek91@gmail.com>
2017-08-16 03:29:56 +0800

    bug fix

--- sma-wire-experiment/arduino_interface.cs
+++ sma-wire-experiment/arduino_interface.cs
@@ -12,81 +12,52 @@ namespace sma_wire_experiment
     public class arduino_interface
     {
         SerialPort currentPort;
-        sma_control_form main_form = new sma_control_form();
+        //sma_control_form main_form = new sma_control_form();
         bool portFound;
 
         int hi = 5;
 
         public int get_hi_val(){ return hi;}
 
-        public string SetComPort()
+
+        public bool SetComPort()
         {
-            string portname = "";
             try
             {
                 string[] ports = SerialPort.GetPortNames();
                 foreach (string port in ports)
                 {
-                    portname = port;
-                    currentPort = new SerialPort(port, 9600);
-                    if (DetectArduino())
-                    {
-                        portFound = true;
-                        break;
-                    }
-                    else
-                    {
-                        portFound = false;
+                    currentPort = new SerialPort(port, 38400);
+                    //if (DetectArduino())
+                    //{
+                    //    portFound = true;
+                    //    break;
+                    //}
+                    //else
+                    //{
+                    //    portFound = false;
 
 
-                    }
+
+                    //}
                 }
             }
             catch (Exception e)
             {
             }
 
-            return portname;
+            return portFound;
         }
 
-        public bool DetectArduino()
+        public void DetectArduino()
         {
             try
             {
-                //The below setting are for the Hello handshake
-                byte[] buffer = new byte[5];
-                buffer[0] = Convert.ToByte(16);
-                buffer[1] = Convert.ToByte(128);
-                buffer[2] = Convert.ToByte(0);
-                buffer[3] = Convert.ToByte(0);
-                buffer[4] = Convert.ToByte(4);
-                int intReturnASCII = 0;
-                char charReturnValue = (Char)intReturnASCII;
-                currentPort.Open();
-                currentPort.Write(buffer, 0, 5);
-                Thread.Sleep(1000);
-                int count = currentPort.BytesToRead;
-                string returnMessage = "";
-                while (count > 0)
-                {
-                    intReturnASCII = currentPort.ReadByte();
-                    returnMessage = returnMessage + Convert.ToChar(intReturnASCII);
-                    count--;
-                }
-                //ComPort.name = returnMessage;
-                currentPort.Close();
-                if (returnMessage.Contains("HELLO FROM ARDUINO"))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+     
             }
             catch (Exception e)
             {
-                return false;
+                //return false;
             }
         }
 
--- sma-wire-experiment/sma_control.cs
+++ sma-wire-experiment/sma_control.cs
@@ -11,16 +11,29 @@ using System.IO.Ports;
 using System.IO;
 using System.Windows.Forms;
 
+
 namespace sma_wire_experiment
 {
     public partial class sma_control_form : Form
     {
+        SerialPort comPort;
         public sma_control_form()
         {
             InitializeComponent();
+            comport_lister();
             arduino_interface arduino = new arduino_interface();
             status_lbl.Text =  arduino.get_hi_val().ToString();
-            status_lbl.Text =  arduino.SetComPort();
+            //status_lbl.Text =  arduino.SetComPort();
+        }
+
+        public void comport_lister()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comport_cb.Items.Add(port);
+                comport_cb.SelectedIndex = 0;
+            }
         }
         
         private void label1_Click(object sender, EventArgs e)
